4bbc3a438112425fa5588c5481ab74fe3aed5814
Endi Daniel <endicoelho@gmail.com>
2019-03-05 11:32:26 -0300

    Separação do formulário em Cliente e Itens,adição do botão Cancelar

--- core/forms.py
+++ core/forms.py
@@ -1,22 +1,24 @@
 from django import forms
-
-class PedidoForm(forms.Form):
-    #Primeiro valor é o valor que sera usado o segundo da tupla e o que sera visto pelo usuario
-    #Clientes Pré Cadastrados
-    CLIENT_CHOICES = ((1,'Darth​ ​Vader'),
+#Clientes Pré Cadastrados
+CLIENT_CHOICES = ((1,'Darth​ ​Vader'),
                      (2,'Obi-Wan​ ​Kenobi'),
                      (3,'Luke​ ​Skywalker'),
                      (4,'Imperador​ ​Palpatine'),
                      (5,'Han​ ​Solo'))
-    #Produtos Pré Cadastrados                 
-    PRODUCT_CHOICES = ((1,'Millenium​ ​Falcon'),
+#Produtos Pré Cadastrados
+PRODUCT_CHOICES = ((1,'Millenium​ ​Falcon'),
                      (2,'X-Wing'),
                      (3,'Super​ ​Star​ ​Destroyer'),
                      (4,'TIE​ ​Fighter'),
                      (5,'Lightsaber'),
                      (6,'DLT-19​ ​Heavy​ ​Blaster​ ​Rifle'),
-                     (7,'DL-44​ ​Heavy​ ​Blaster​ ​Pistol'))                  
+                     (7,'DL-44​ ​Heavy​ ​Blaster​ ​Pistol'))
+
+class ClientForm(forms.Form):
+    #Primeiro valor é o valor que sera usado o segundo da tupla e o que sera visto pelo usuario
     clientes = forms.ChoiceField(choices = CLIENT_CHOICES)
+class ItemForm(forms.Form):
+    #Primeiro valor é o valor que sera usado o segundo da tupla e o que sera visto pelo usuario                      
     produtos = forms.ChoiceField(choices = PRODUCT_CHOICES)
     quantidade = forms.IntegerField(min_value=0)
     preco = forms.FloatField(min_value=0)
--- core/views.py
+++ core/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .forms import PedidoForm
+from .forms import ClientForm,ItemForm
 from .models import Item
 
 #constantes
 from .constantes import endi
 
-# Create your views here.
+#################funcoes para tratar e manipular os dados#################3
+
+
+################# Views abaixo ################
+
 #renderiza a página inicial
 def index(request):
     return render(request,'core/index.html')
@@ -16,25 +20,35 @@ def index(request):
 def novoPedido(request):
     #se a requisição chega com o verbo POST processa os dados recebidos e realiza as operações necessárias
     if request.method == 'POST':
-        form = PedidoForm(request.POST)
-        if form.is_valid():
+        
+        form1 = ClientForm(request.POST)
+        form2 = ItemForm(request.POST)
+        if (form1.is_valid() & form2.is_valid()):
+            
+            
+
             #processa os dados do formulario  e redireciona para alguma url
-            client = form.cleaned_data['clientes']
-            product = form.cleaned_data['produtos']
-            quantity = form.cleaned_data['quantidade']
-            price = form.cleaned_data['preco']
+            client = form1.cleaned_data['clientes']
+            product = form2.cleaned_data['produtos']
+            quantity = form2.cleaned_data['quantidade']
+            price = form2.cleaned_data['preco']
             eu = endi['daniel']
+
             #daqui pra baix realizar operacoes com os dados obtidos pelo formulario
             #criar o item no banco de dados
-            #i = Item(quantidade_digitada_pelo_usuario=quantity,preco_digitado_pelo_usuario=price,)
+            # = Item(quantidade_digitada_pelo_usuario=quantity,preco_digitado_pelo_usuario=price,)
             #antes de salvar o item realizar as validações
             
             context = {'client':client,'product':product,'quantity':quantity,'price':price,'eu':eu}
             return render(request,'core/resultado.html',context)
     else:
         #caso a requisição não chegue como POST renderiza o formulario no formto padrão para o usuário
-        form = PedidoForm()
-    return render(request,'core/novoPedido.html',{'form':form})
+        #se cancelar volta pra página inicial
+        if('Cancelar' in request.GET):
+            return HttpResponseRedirect(reverse('core:index'))
+        form1 = ClientForm()
+        form2 = ItemForm()
+    return render(request,'core/novoPedido.html',{'form1':form1,'form2':form2})
 
 def resultadoPedido(request):
     return render(request,'core/resultado.html')    
