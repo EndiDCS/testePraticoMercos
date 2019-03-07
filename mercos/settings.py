348f5daada1214b1459a8761b7cb7ba80674137e
Endi Daniel <endicoelho@gmail.com>
2019-03-07 20:51:57 -0300

    Deploy Concluido

--- mercos/settings.py
+++ mercos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['teste-pratico-mercos-endi.herokuapp.com']
+ALLOWED_HOSTS = ['teste-pratico-mercos-endi.herokuapp.com','localhost']
 
 
 # Application definition
