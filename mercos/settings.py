5b6835d4227ee6861311e4e3ef4d23f025c858a4
Endi Daniel <endicoelho@gmail.com>
2019-03-19 15:08:47 -0300

    mudanças no settings

--- mercos/settings.py
+++ mercos/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
+#SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '(*7zw-_p+u1&2s@d#=0hg3ycp1mo^@v*nl+x@uh6dccmpxpkxa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
