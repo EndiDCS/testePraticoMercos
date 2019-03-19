dd1bf66b96505df99b318f8e43992918a7b00b45
Endi Daniel <endicoelho@gmail.com>
2019-03-19 12:51:02 -0300

    voltando debug config

--- mercos/settings.py
+++ mercos/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 ALLOWED_HOSTS = ['teste-pratico-mercos-endi.herokuapp.com','localhost']
 
 
